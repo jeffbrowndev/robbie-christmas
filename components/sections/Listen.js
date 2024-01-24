273462a79f9e7578d33a0984e58f0475764db524
jeffbrowndev <jeffbrowndev@gmail.com>
2024-01-24 15:05:24 -0800

    Adds audio samples

--- components/sections/Listen.js
+++ components/sections/Listen.js
@@ -3,6 +3,8 @@ import MaxWidthContainer from "../layouts/MaxWidthContainer";
 import { useContext } from "react";
 import { DataContext } from "@/context/DataContext";
 import Video from "@/components/common/Video";
+import playAudio from "@/public/images/play-audio.png";
+import Image from "next/image";
 
 const Listen = () => {
   const { videos } = useContext(DataContext);
@@ -11,6 +13,92 @@ const Listen = () => {
     <MaxWidthContainer>
       <div className={styles['listen-content']}>
         <h2>Listen</h2>
+        <div className={styles['audio']}>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+          <div className={styles['song']}>
+            <Image alt='play audio' src={playAudio} className={styles['play-button']} />
+            <div className={styles['audio-data']}>
+              <h3>Song Name</h3>
+              <p>Artist Name</p>
+            </div>
+          </div>
+        </div>
         <div className={styles['video-gallery']}>
           {videos.map(video => <Video video={video} key={video.src} />)}
         </div>
