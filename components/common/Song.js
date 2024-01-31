6557a7d74bcdfc0e9ac83448560a997aed30ff9b
jeffbrowndev <jeffbrowndev@gmail.com>
2024-01-31 10:55:25 -0800

    Finished song list

--- components/common/Song.js
+++ components/common/Song.js
@@ -2,10 +2,7 @@ import styles from "@/styles/songList.module.scss";
 
 const Song = ({ song, addToPlaylist }) => {
   return (
-    <div className={styles['song']}>
-      {/* <div className={styles['playlist-column']}>
-        <input defaultChecked={song.playlist} type='checkbox' onChange={(e) => addToPlaylist(song.index, e.currentTarget.checked)} />
-      </div> */}
+    <div className={`${styles['song']} ${song.playlist ? styles['playlist'] : ''}`} onClick={() => addToPlaylist(song.index, song.playlist)}>
       <p className={styles['artist-column']}>{song.artist}</p>
       <p className={styles['title-column']}>{song.title}</p>
       <p className={styles['genre-column']}>{song.genre}</p>
--- components/sections/SongList.js
+++ components/sections/SongList.js
@@ -4,7 +4,7 @@ import styles from "@/styles/songList.module.scss";
 import { useContext, useState } from "react";
 import Song from "../common/Song";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDownAZ } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDownAZ, faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { jsPDF } from "jspdf";
 import autoTable from "jspdf-autotable";
 
@@ -14,9 +14,8 @@ const SongList = () => {
   const [allSongs, setAllSongs] = useState(songs);
   
   // filters
-  const [genre, setGenre] = useState("All");
+  const [genre, setGenre] = useState("All genres");
   const [searchText, setSearchText] = useState('');
-  const [hasAudio, setHasAudio] = useState(false);
   const [playlistOnly, setPlaylistOnly] = useState(false);
 
   const sort = (type) => {
@@ -41,7 +40,7 @@ const SongList = () => {
     const copy = [...allSongs];
     const song = copy.find(song => song.index === index);
 
-    song.playlist = addToPlaylist;
+    song.playlist = !addToPlaylist;
 
     setAllSongs(copy);
   };
@@ -60,10 +59,7 @@ const SongList = () => {
     if (!songText.includes(searchText.toLowerCase())) {
       return true;
     }
-    if (song.genre !== genre && genre !== "All") {
-      return true;
-    }
-    if (hasAudio && !song.audio) {
+    if (song.genre !== genre && genre !== "All genres") {
       return true;
     }
     if (playlistOnly && !song.playlist) {
@@ -87,27 +83,32 @@ const SongList = () => {
     pdf.save("playlist.pdf");
   }
 
+  const handlePlaylistOnly = (value) => value === "Playlist" ? setPlaylistOnly(true) : setPlaylistOnly(false);
+
   return (
     <MaxWidthContainer>
       <div id='song-list' className={styles['song-list']}>
         <div className={styles['song-list-header']}>
           <h2>Song List</h2>
-          <p className="main-text">Explore over 600 popular songs or select your favorite songs to create and download a playlist.</p>
+          <p className="main-text">Explore over 600 popular songs or select your favorites to create and download a customized&nbsp;playlist.</p>
         </div>
         <div className={styles['song-list-container']}>
           <div className={styles['controls']}>
             <div className={styles['search']}>
-            <h3>Search</h3>
               <div className={styles['input-row']}>
                 <input onChange={(e) => setSearchText(e.currentTarget.value)} className={styles['search-box']} placeholder="Search by artist, song, or genre" />
                 <div className={styles['controls-right']}>
                   <div className={styles['filters']}>
-                    <input className={styles['checkbox']} type='checkbox' onChange={(e) => setPlaylistOnly(e.currentTarget.checked)} />
-                    <label>Playlist only</label>
+                    <select defaultValue='All genres' onChange={(e) => setGenre(e.currentTarget.value)}>
+                      <option value='All genres'>All genres</option>
+                      {getGenres().map(genre => <option>{genre}</option>)}
+                    </select>
+                    <select defaultValue='All songs' onChange={(e) => handlePlaylistOnly(e.currentTarget.value)}>
+                      <option value='All songs'>All songs</option>
+                      <option value='Playlist'>Playlist</option>
+                    </select>
                   </div>
-                  <button onClick={() => downloadPlaylist()} className={styles['accent-button']}>
-                    DOWNLOAD PLAYLIST (.pdf)
-                  </button>
+                  <FontAwesomeIcon title="Download Playlist (.pdf)" className={styles['download']} icon={faFileArrowDown} onClick={() => downloadPlaylist()} />
                 </div>
               </div>
             </div>
@@ -123,10 +124,6 @@ const SongList = () => {
             </div>
             <div class={styles['genre-column']}>
               <span>Genre</span>
-              <select onChange={(e) => setGenre(e.currentTarget.value)}>
-                <option selected>All</option>
-                {getGenres().map(genre => <option>{genre}</option>)}
-              </select>
             </div>
           </div>
           <div className={styles['scrolling-song-list']}>{displaySongs()}</div>
