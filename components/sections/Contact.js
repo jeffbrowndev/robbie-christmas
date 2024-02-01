ed1be4cfab37b493996ab35cf2fe74545d59a393
jeffbrowndev <jeffbrowndev@gmail.com>
2024-02-01 10:17:36 -0800

    Adds email copy button and :mailto

--- components/sections/Contact.js
+++ components/sections/Contact.js
@@ -4,16 +4,23 @@ import styles from "@/styles/contact.module.scss";
 import Image from "next/image";
 import rxLogo from "@/public/images/rx-logo-footer.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faSpotify, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import AccentButton from "../common/AccentButton";
 import Link from "next/link";
 
 const Contact = () => {
+  const email = "robbiechristmasmusic@gmail.com";
 
   const scroll = (dest) => {
     const target = document.getElementById(dest);
 
     target.scrollIntoView();
+  }
+
+  const copyEmail = () => {
+    navigator.clipboard.writeText(email);
+
+    window.alert("E-mail copied to clipboard")
   }
 
   return (
@@ -26,13 +33,18 @@ const Contact = () => {
               <AccentLine />
               <p className={styles['p-large']} >For all inquiries, booking, or to request a quote, please contact the e-mail address below and allow 24-48 hours for a response.</p>
             </div>
-            <p className={styles['e-mail']} >robbiechristmasmusic@gmail.com</p>
+            <div className={styles['email-container']}>
+              <Link style={{textDecoration: 'none'}} href={`mailto:${email}'`}>
+                <p className={styles['email']}>{email}</p>
+              </Link>
+              <FontAwesomeIcon title='Copy e-mail' className={styles['icon']} onClick={() => copyEmail()} icon={faCopy} />
+            </div>
             <Image className={styles['contact-image-desktop']} src={rxLogo} alt="rx logo" />
           </div>
           <div className={styles['contact-content-right']}>
           <div className={styles['social-media']}>
               <h3>Consulting</h3>
-              <p className={styles['p-large']} >Want to talk it through before your big event? Schedule a consultation today to walk through the details.</p>
+              <p className={styles['p-large']} >Want to talk it through before your big event? Schedule a free consultation today to walk through the details.</p>
               <Link href="https://robbiechristmasmusic241689.hbportal.co/schedule/656a354a96d85800394d945a" target="_blank">
                 <button className={styles['accent-button']}>
                   SCHEDULE CONSULTATION
